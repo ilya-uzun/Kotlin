31dad068cc47bd62225ffab633ab5a228dcc133e
ilya-uzun <ilya.uzun@bk.ru>
2021-05-20 22:25:24 +0500

    class Coorinate

---
+++ Sudoku/src/main/kotlin/com/ilya/sudoku/Coorinate.kt
@@ -0,0 +1,7 @@
+package com.ilya.sudoku
+
+class Coorinate (var x: Int, var y: Int){
+    override fun toString(): String {
+        return "[$x,$y]"
+    }
+}
--- Sudoku/src/main/kotlin/com/ilya/sudoku/main.kt
+++ Sudoku/src/main/kotlin/com/ilya/sudoku/main.kt
@@ -1,4 +1,5 @@
 @file:JvmName("Main")
+
 package com.ilya.sudoku
 
 import java.io.File
@@ -7,14 +8,14 @@ import java.io.InputStream
 fun main() {
 
     val input = readInput()
+    val solution = readSolution()
     //val input: InputStream = File("input").inputStream()
     //val inputString = input.bufferedReader().use { it.readText() }
     println(input)
-
+    println(solution)
 }
 
-fun readInput():Map<Pair<Int, Int>, Int>{
-
+fun readInput():Map<Coorinate, Int>{
     val input = File("input")
         .readLines()
         .withIndex()
@@ -24,8 +25,27 @@ fun readInput():Map<Pair<Int, Int>, Int>{
                 .filter { indexedChar -> indexedChar.value != '.'} // != добавели фильтр точекр
                 .map{ indexedChar ->
                     val yCoordinate = indexedChar.index
-                    var number = Character.getNumericValue(indexedChar.value)
-                    (xCoordinate to yCoordinate) to number
+                    val coordinate = Coorinate(xCoordinate, yCoordinate)
+                    val number = Character.getNumericValue(indexedChar.value)
+                    coordinate to number
+                }
+        }
+        .toMap()
+    return  input
+}
+fun readSolution():Map<Coorinate, Int>{
+    val input = File("solution")
+        .readLines()
+        .withIndex()
+        .flatMap { indexedValue ->
+            val xCoordinate = indexedValue.index
+            indexedValue.value.toCharArray().withIndex()
+                .filter { indexedChar -> indexedChar.value != '.'} // != добавели фильтр точекр
+                .map{ indexedChar ->
+                    val yCoordinate = indexedChar.index
+                    val coordinate = Coorinate(xCoordinate, yCoordinate)
+                    val number = Character.getNumericValue(indexedChar.value)
+                    coordinate to number
                 }
         }
         .toMap()
