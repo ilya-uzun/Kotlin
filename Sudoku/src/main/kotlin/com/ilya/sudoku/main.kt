bc0a2e7f2c771b459cd19e52dd7cfb2b95806ffe
ilya-uzun <ilya.uzun@bk.ru>
2021-05-21 22:20:59 +0500

    fun readFile

--- Sudoku/src/main/kotlin/com/ilya/sudoku/main.kt
+++ Sudoku/src/main/kotlin/com/ilya/sudoku/main.kt
@@ -7,16 +7,16 @@ import java.io.InputStream
 
 fun main() {
 
-    val input = readInput()
-    val solution = readSolution()
+    val input = readFile("input")
+    val solution = readFile("solution")
     //val input: InputStream = File("input").inputStream()
     //val inputString = input.bufferedReader().use { it.readText() }
     println(input)
     println(solution)
 }
 
-fun readInput():Map<Coorinate, Int>{
-    val input = File("input")
+fun readFile(fileName: String):Map<Coorinate, Int> =
+    File(fileName)
         .readLines()
         .withIndex()
         .flatMap { indexedValue ->
@@ -31,23 +31,5 @@ fun readInput():Map<Coorinate, Int>{
                 }
         }
         .toMap()
-    return  input
-}
-fun readSolution():Map<Coorinate, Int>{
-    val input = File("solution")
-        .readLines()
-        .withIndex()
-        .flatMap { indexedValue ->
-            val xCoordinate = indexedValue.index
-            indexedValue.value.toCharArray().withIndex()
-                .filter { indexedChar -> indexedChar.value != '.'} // != добавели фильтр точекр
-                .map{ indexedChar ->
-                    val yCoordinate = indexedChar.index
-                    val coordinate = Coorinate(xCoordinate, yCoordinate)
-                    val number = Character.getNumericValue(indexedChar.value)
-                    coordinate to number
-                }
-        }
-        .toMap()
-    return  input
-}
+
+
