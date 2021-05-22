413281e47711c1f47e19a516fc0b186bbb90d1fc
ilya-uzun <ilya.uzun@bk.ru>
2021-05-22 17:37:31 +0500

    map test

---
+++ test/map.kt
@@ -0,0 +1,13 @@
+
+fun main() {
+    val map = mapOf(
+        "John" to "Audi",
+        "Bill" to "BMW",
+        "Michael" to "Chevrolet"
+        )
+
+    println(map)
+    println(map["Bill"])
+}
+
+
