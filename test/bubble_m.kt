cb00d1a110b1a7b5b0f0bc2eb4c4a14c5df70ebb
ilya-uzun <ilya.uzun@bk.ru>
2021-05-28 22:43:10 +0500

    Проработать сортировку

--- test/bubble_m.kt
+++ test/bubble_m.kt
@@ -1,17 +1,31 @@
 fun main(args: Array<String>) {
     println ("Введите число")
-    val size = readLine()!!.toInt() // !!.toInt() - преобразование из Strung
+    var size = readLine()!!.toInt() // !!.toInt() - преобразование из Strung
     println ("\n")
 
     // Инициализация массива способ 1
     // var arrayRandom = Array (size, {(0 until 10).random()})
     // for ((index, value) in arrayRandom.withIndex()) {
-    //     print("$value")
+    //     println("$index - $value ")
     // } 
-    
+
     // Инициализация массива способ 1
     var arrayRandom = Array (size, {(0 until 10).random()})
+
     for (i in arrayRandom.indices) {
+        print("${arrayRandom[i]} ")
+    }
+
+    for (i < (size - 1)){
+        for (j > (size + 1))
+            if(arrayRandom[j] > arrayRandom[j]){
+                var tepm = arrayRandom[j] 
+                arrayRandom[j]= arrayRandom[j+1]
+                arrayRandom[j+1] = tepm
+            }
+    }
+
+       for (i in arrayRandom.indices) {
         print("${arrayRandom[i]} ")
     }
 }
