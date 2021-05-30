e664b0fcb12d9ff338c59638793fec382525df5e
ilya-uzun <ilya.uzun@bk.ru>
2021-05-30 19:24:14 +0500

    Finish

--- test/bubble_m.kt
+++ test/bubble_m.kt
@@ -2,14 +2,12 @@ fun main(args: Array<String>) {
     println ("Введите число")
     var size = readLine()!!.toInt() // !!.toInt() - преобразование из Strung
     println ("\n")
-
-    // Инициализация массива способ 1
-    // var arrayRandom = Array (size, {(0 until 10).random()})
-    // for ((index, value) in arrayRandom.withIndex()) {
-    //     println("$index - $value ")
-    // } 
-
-    // Инициализация массива способ 1
+    // проверка на отричательное значение
+    if (size <= 0){
+        println("Введенно недопустимое значение")
+        return 
+    }
+    // Инициализация массива
     var arrayRandom = Array (size, {(0 until 10).random()})
 
     for (i in arrayRandom.indices) {
@@ -17,10 +15,8 @@ fun main(args: Array<String>) {
     }
     print("\n")
 
-	val a = size - 1
-    for (i in 1..a){
-        val b = size - i - 1
-        for (j in 0..b)
+    for (i in 1..size - 1){
+        for (j in 0..size - i - 1)
             if(arrayRandom[j] > arrayRandom[j+1]){
                 var tepm = arrayRandom[j] 
                 arrayRandom[j]= arrayRandom[j+1]
@@ -28,7 +24,7 @@ fun main(args: Array<String>) {
             }
     }
 
-       for (i in arrayRandom.indices) {
+    for (i in arrayRandom.indices) {
         print("${arrayRandom[i]} ")
     }
     print("\n")
