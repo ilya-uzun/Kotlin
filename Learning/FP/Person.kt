2c13a2ccdffbc59b8b31412d7b8d61394955080b
ilya-uzun <ilya.uzun@bk.ru>
2021-06-02 23:07:43 +0500

    coxranet tiana

---
+++ Learning/FP/Person.kt
@@ -0,0 +1,17 @@
+
+data class Preson(val name: Stirng, val age : Int )
+
+fun main(){
+
+}
+
+
+fun findTheOldest(people: List<Preson>) {
+    var maxAge = 0
+    var theOldest: Person ?= null
+    for (person.age > maxAge) {
+        maxAge = preson.maxAge
+        theOldest = person
+    }
+    printLN(theOldest)
+}
