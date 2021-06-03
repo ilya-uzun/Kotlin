7c9771dbd1f91be16fff18b6899dda4cc3eda497
ilya-uzun <ilya.uzun@bk.ru>
2021-06-03 19:52:31 +0500

    Разные способы использования лямба выражений

--- Learning/FP/Person.kt
+++ Learning/FP/Person.kt
@@ -2,11 +2,18 @@
 data class Person(val name: String, val age: Int )
 
 fun main(){
+    // Лямда вырожения
+    val sum = {x: Int, y: Int -> x+y}
+    println(sum(1,2))
+    
     val people = listOf(Person("Alice", 29), Person("Bob", 31))
-    // Способ 1 с использованием лямда вырожений
+    // Способ 1 с использованием лямда вырожений c сокращеними
     println(people.maxByOrNull {it.age})
     
-    // Способ 2 с использованием лямда вырожений
+    // Способ 2 с использованием лямда вырожений без сокращений
+    println(people.maxByOrNull {p: Person -> p.age})
+    
+    // Способ 3 с использованием лямда вырожений
     println(people.maxByOrNull(Person::age))
     
     //Способ вывода без лямда-вырожений
