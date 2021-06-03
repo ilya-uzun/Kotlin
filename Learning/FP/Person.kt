d0e20003a243039228a9b50187b6eab96a6ea493
ilya-uzun <ilya.uzun@bk.ru>
2021-06-03 15:56:00 +0500

    лямда выражения

--- Learning/FP/Person.kt
+++ Learning/FP/Person.kt
@@ -1,11 +1,18 @@
 
-data class Person(val name: String, val age : Int )
+data class Person(val name: String, val age: Int )
 
 fun main(){
     val people = listOf(Person("Alice", 29), Person("Bob", 31))
-    findTheOldest(people)
+    // Способ 1 с использованием лямда вырожений
+    println(people.maxByOrNull {it.age})
+    
+    // Способ 2 с использованием лямда вырожений
+    println(people.maxByOrNull(Person::age))
+    
+    //Способ вывода без лямда-вырожений
+    //findTheOldest(people)
 }
-
+/*
 fun findTheOldest(people: List<Person>) {
     var maxAge = 0
     var theOldest: Person ?= null
@@ -16,4 +23,4 @@ fun findTheOldest(people: List<Person>) {
         }
     }
     println(theOldest)
-}
+}*/
