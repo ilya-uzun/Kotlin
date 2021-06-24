92c4e1168dd7a98582f43616376d744b17abd0ff
Илья <63155683+ilya-uzun@users.noreply.github.com>
2021-06-24 10:36:57 +0500

    Update summa_to_one.kt
    Работает

--- Learning/summa_to_one.kt
+++ Learning/summa_to_one.kt
@@ -1,13 +1,14 @@
 fun main(){
   println("Введите число")
-  val n = readLine()!!.toInt()
-  val m = readLine()!!.toInt()
-  println(sum(n,m))
+   val n = 6 //readLine()!!.toInt() // онлайн компилятор выдает ошибку
+   val m = 3 //readLine()!!.toInt()
+   println("$n, $m")
+   println(sum(n,m))
 }
 
 fun sum(n: Int, m: Int): Int {
-  var summa
-  for (i in n) summa++
-  if (n==summa) for (j in m) summa++
-  return summa
+  var s = 0
+  for (i in 0 until n) s++
+  if (n==s) for (j in 0 until m) s++
+  return s
 }
