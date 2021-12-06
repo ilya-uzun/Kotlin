713f0a5d8bf1aa2a24152edcb8bab34452e3f2f8
Илья <63155683+ilya-uzun@users.noreply.github.com>
2021-12-06 16:26:30 +0500

    Update array.kt

--- Learning/Arrey/array.kt
+++ Learning/Arrey/array.kt
@@ -1,7 +1,12 @@
 fun main(){
     val fiveFives = Array(5,{5})
-    for(i in fiveFives){
-        println(fiveFives[i])
+    for (i in fiveFives.indices) print("${arrayRandom[i]} ")
+   
    }
     println(fiveFives[0])
+}
+
+fun printArray(a: Int, b: Int): Array {
+    val fiveFives = Array(a,{b})
+    
 }
