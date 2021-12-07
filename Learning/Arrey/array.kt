7e273c12dfc4eca834419dfab3cc34c02d962f90
Илья <63155683+ilya-uzun@users.noreply.github.com>
2021-12-07 08:09:44 +0500

    Update array.kt

--- Learning/Arrey/array.kt
+++ Learning/Arrey/array.kt
@@ -4,9 +4,36 @@ fun main(){
    
    }
     println(fiveFives[0])
+    
+    fun isSorted(a: IntArray): Unit {
+    for (i in 0..a.size - 1) {
+    if (a [i]> a [i + 1]) 
+    return println ("NO")
+} 
+    return println ("YES")
+    
 }
 
 fun printArray(a: Int, b: Int): Array {
     val fiveFives = Array(a,{b})
     
+}
+
+
+fun main() {
+
+} 
+
+fun createArray(number: Int): IntArray {
+    val scanner = Scanner (System.`in`) 
+    val array = IntArray (number) 
+    for (i in 1..number) {
+        array [i - 1] = number.nextInt()
+    } 
+    return array
+} 
+fun main() {
+    val scanner = Scanner (System.`in`) 
+    val n = scanner.nextInt() 
+    val sortedArray = createArray(n) isSorted(sortedArray)}
 }
