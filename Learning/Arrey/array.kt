4bbb178fad459e9eb4b4c69a594c1284671eb704
Илья <63155683+ilya-uzun@users.noreply.github.com>
2021-12-08 15:45:41 +0500

    Update array.kt
    Работает вывод массива через функцию

--- Learning/Arrey/array.kt
+++ Learning/Arrey/array.kt
@@ -1,39 +1,41 @@
-fun main(){
-    val fiveFives = Array(5,{5})
-    for (i in fiveFives.indices) print("${arrayRandom[i]} ")
-   
-   }
-    println(fiveFives[0])
+fun isSorted(a: Array<Int>, size: Int): Unit {
     
-    fun isSorted(a: IntArray): Unit {
-    for (i in 0..a.size - 1) {
-    if (a [i]> a [i + 1]) 
-    return println ("NO")
-} 
-    return println ("YES")
-    
-}
-
-fun printArray(a: Int, b: Int): Array {
-    val fiveFives = Array(a,{b})
-    
-}
-
-
-fun main() {
-
-} 
-
-fun createArray(number: Int): IntArray {
-    val scanner = Scanner (System.`in`) 
-    val array = IntArray (number) 
-    for (i in 1..number) {
-        array [i - 1] = number.nextInt()
+    for(i in 0..a.size - 1){
+        if(a[i]>a[i + 1]){
+            var j = 0 
+            j++  
+        }
     } 
-    return array
+    
+    println ("size $size")
+    println ("a.size $a.size")
+    
+    if (a.size == size) {
+        return println ("YES")
+    } else {
+        return println ("NO")
+    }  
 } 
+// 
+fun createArray(number: Int): Array<Int>{
+    //val scanner = Scanner(System.`in`) 
+    //val array = IntArray(number) 
+
+    val a: Array<Int> = Array(3) {0}
+
+    for (i in 0 until number){
+        //array[i - 1] = number.nextInt()
+        a[i] = (Math.random() * 10).toInt()
+        print(" ${a[i]}")
+    } 
+    return a
+} 
+
 fun main() {
-    val scanner = Scanner (System.`in`) 
-    val n = scanner.nextInt() 
-    val sortedArray = createArray(n) isSorted(sortedArray)}
+    //val scanner = Scanner (System.`in`) 
+    //val n = scanner.nextInt() 
+    val n = 3
+    val sortedArray = createArray(n) 
+    val outAnswer = isSorted(sortedArray, n)
 }
+
