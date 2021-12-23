0be5fee8ad1e502322b4c9493a266f83a2ff6f9a
ilya-uzun <ilya.uzun@bk.ru>
2021-12-23 20:51:03 +0500

    FizzBuzz.kt ошибка запеси массива

--- Learning/Arrey/array.kt
+++ Learning/Arrey/array.kt
@@ -49,20 +49,3 @@ outAnswer
 }
 
 
-fun fizzBuzz(n: Int): Array<String> {
-    var a = Array(n){i -> (1+i).toString()}
-    
-    a.forEachIndexed{ i, element -> 
-        if (i % 3 == 0 && i % 5 == 0) a[i] = "FizzBuzz"
-        if (i % 3 == 0) a[i] = "Fizz"
-        if (i % 5 == 0) a[i] = "Buzz"
-    }
-    
-    return a
-}
-
-fun main() {
-    val numbersMap = fizzBuzz(10)
-    for(i in numbersMap.indices) println(numbersMap[i])
-    
-}
