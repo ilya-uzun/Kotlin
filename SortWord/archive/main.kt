21912f0eb8db2793e8920312f37724404681f4dc
ilya-uzun <ilya.uzun@bk.ru>
2022-01-02 16:05:34 +0500

    Файл переписывает но без пробелов

---
+++ SortWord/archive/main.kt
@@ -0,0 +1,26 @@
+import java.io.File
+/**
+ *
+ * inputName имя первоначального файла с текстом.
+ * outputName имя переписанного файла с текстом из файла  inputName.
+ */
+class Sort(){
+
+    fun writeFile(inputName: String,  outputName: String){
+        val writer = File(outputName).bufferedWriter()
+        for (line in File(inputName).readLines()) {
+            if (line.isEmpty()) {
+                writer.newLine()
+            }
+            for (word in line.split(Regex("\\s"))) {
+                writer.write(word)
+            }
+        writer.close()
+    }
+}
+
+fun main(){
+    val sort = Sort()
+    sort.writeFile("test1", "test2")
+
+}
