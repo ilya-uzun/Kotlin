c90b4e7d2641c97a8f087365949db9be48a31ad0
ilya-uzun <ilya.uzun@bk.ru>
2022-01-02 17:22:00 +0500

    Запись без ошибок, полное копирование файла

--- SortWord/archive/main.kt
+++ SortWord/archive/main.kt
@@ -6,23 +6,36 @@ import java.io.File
  */
 class Sort(){
 
+    fun readFile(inputName: String): String = File(inputName).readText()
+
+    fun writeFile(text: String, outputName: String){
+        val f = File(outputName)
+            if (!f.exists()) {
+                f.createNewFile()
+            } 
+        f.writeText(text)
+   }
+
+/*
+    fun writeFile(outputName: String){
+    }
+
     fun writeFile(inputName: String,  outputName: String){
         val writer = File(outputName).bufferedWriter()
         for (line in File(inputName).readLines()) {
-            if (line.isEmpty()) {
-                writer.newLine()
-            }
-            for (word in line.split(Regex("\\s"))) {
+
+            for (word in line.split(Regex("\n"))) {
                 writer.write(" ")
                 writer.write(word)
             }
         writer.close()
        }
-   }
+    }
+*/
 }
 
 fun main(){
     val sort = Sort()
-    sort.writeFile("test1", "test2")
-
+    
+    sort.writeFile(sort.readFile("test1"), "test2" )
 }
