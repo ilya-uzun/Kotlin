865715570bc840292c4cf1b4d7ee363dc55cc298
Илья <63155683+ilya-uzun@users.noreply.github.com>
2022-02-10 15:08:41 +0500

    Update Matrix.kt

--- Learning/Arrey/Matrix.kt
+++ Learning/Arrey/Matrix.kt
@@ -14,3 +14,17 @@ fun main(){
     }
 }
 
+
+// Data class с структурай матрици
+data class Cell(val row: Int, val column: Int)
+ 
+interface Matrix<E> {
+    val height: Int
+    val width: Int
+ 
+    operator fun get(row: Int, column: Int): E
+    operator fun get(cell: Cell): E
+ 
+    operator fun set(row: Int, column: Int, value: E)
+    operator fun set(cell: Cell, value: E)
+}
